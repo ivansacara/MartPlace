826e4e9d55e2ae588c57210f915266ededd858be
ivansacara <ivansacara@gmail.com>
2020-12-23 16:55:01 +0200

    macked home page

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,27 @@ $(function () {
 
 
 
+	$('.slider-inner').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-right"></span></button>',
+		nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-left"></span></button>'
+	});
 
+	$('.follower_slider').slick({
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		prevArrow: '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-right"></span></button>',
+		nextArrow: '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-left"></span></button>'
+
+
+	});
+
+
+	$(".rate-star").rateYo({
+		rating: 4.5,
+		readOnly: true,
+		starWidth: "15px"
+	});
+	var mixer = mixitup('.new_items-slider');
 });
